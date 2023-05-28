aefb5826bcf3ef8fca1cf8f09c0542f8e06a78cc
ooo <AnthonyCelio@gmail.com>
2023-05-27 21:20:45 -0400

    content

--- src/content.js
+++ src/content.js
@@ -7,15 +7,21 @@ const MyContent = () => {
   return (
     <div className="container">
       <div className="image-box">
-        <img src={image1} alt="GPU Cloud Computing Service" href="https://stratamos.com"/>
+        <a href="https://stratamos.com" target="_blank" rel="noopener noreferrer">
+          <img src={image1} alt="GPU Cloud Computing Service"/>
+        </a>
         <p>Full stack site I built from scratch with MERN to sell time on VMs with GPUs for ML.</p>
       </div>
       <div className="image-box">
-        <img src={image2} alt="The MPO Algorithm Written in C++" href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp"/>
+        <a href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp" target="_blank" rel="noopener noreferrer">
+          <img src={image2} alt="The MPO Algorithm Written in C++"/>
+        </a>
         <p>Implementation of the MPO (RL) algorithm in C++. Most of the challenge was rewriting parts of the Pytorch library in C++.</p>
       </div>
       <div className="image-box">
-        <img src={image3} alt="A front end site" href="https://jjmobility.net"/>
+        <a href="https://jjmobility.net" target="_blank" rel="noopener noreferrer">
+          <img src={image3} alt="A front end site"/>
+        </a>
         <p>Front-end React site I made for a local business with about 100 pages.</p>
       </div>
     </div>
