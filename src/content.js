020e8f7ce1085e8c8a985264f4a8d8882ff88309
ooo <AnthonyCelio@gmail.com>
2023-05-27 20:53:50 -0400

    content

--- src/content.js
+++ src/content.js
@@ -4,16 +4,16 @@ const MyContent = () => {
   return (
     <div className="container">
       <div className="image-box">
-        <img src="path-to-your-image1.jpg" alt="Your image description" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <img src="./Images/Stratamos.png" alt="GPU Cloud Computing Service" href="stratamos.com"/>
+        <p>Full stack site I built from scratch with MERN to sell time on VMs with GPUs for ML.</p>
       </div>
       <div className="image-box">
-        <img src="path-to-your-image2.jpg" alt="Your image description" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <img src="CMPO.png" alt="The MPO Algorithm Written in C++" href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp"/>
+        <p>I implemented the RL, MPO algorithm in C++. I ended up writing a few things in C++ that Pytorch only had for Python.</p>
       </div>
       <div className="image-box">
-        <img src="path-to-your-image3.jpg" alt="Your image description" />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <img src="JJMobility.png" alt="A front end site" href="jjmobility.net"/>
+        <p>Front-end React site with about 100 pages.</p>
       </div>
     </div>
   );
