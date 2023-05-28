553f0b33f3f6b0fe3681395395eda5b895b75f8f
ooo <AnthonyCelio@gmail.com>
2023-05-27 21:13:57 -0400

    content

--- src/content.js
+++ src/content.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import image1 from './images/Stratamos.png';
+import image2 from './images/CMPO.png';
+import image3 from './images/JJMobility.png';
 
 const MyContent = () => {
   return (
     <div className="container">
       <div className="image-box">
-        <img src="./images/Stratamos.png" alt="GPU Cloud Computing Service" href="https://stratamos.com"/>
+        <img src={image1} alt="GPU Cloud Computing Service" href="https://stratamos.com"/>
         <p>Full stack site I built from scratch with MERN to sell time on VMs with GPUs for ML.</p>
       </div>
       <div className="image-box">
-        <img src="./images/CMPO.png" alt="The MPO Algorithm Written in C++" href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp"/>
-        <p>I implemented the RL, MPO algorithm in C++. I ended up writing a few things in C++ that Pytorch only had for Python.</p>
+        <img src={image2} alt="The MPO Algorithm Written in C++" href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp"/>
+        <p>Implementation of the MPO (RL) algorithm in C++. Most of the challenge was rewriting parts of the Pytorch library in C++.</p>
       </div>
       <div className="image-box">
-        <img src="./images/JJMobility.png" alt="A front end site" href="https://jjmobility.net"/>
-        <p>Front-end React site with about 100 pages.</p>
+        <img src={image3} alt="A front end site" href="https://jjmobility.net"/>
+        <p>Front-end React site I made for a local business with about 100 pages.</p>
       </div>
     </div>
   );
