6851dc762963968c0800f22b26973eda9c69be40
ooo <AnthonyCelio@gmail.com>
2023-05-28 17:06:46 -0400

    content

--- src/content.js
+++ src/content.js
@@ -10,19 +10,19 @@ const MyContent = () => {
         <a href="https://stratamos.com" target="_blank" rel="noopener noreferrer">
           <img src={image1} alt="GPU Cloud Computing Service"/>
         </a>
-        <p>Full stack site I built from scratch with MERN to sell time on VMs with GPUs.</p>
+        <p>Full stack site I built from scratch with MERN to sell time on VMs with GPUs</p>
       </div>
       <div className="image-box">
         <a href="https://github.com/MotorCityCobra/C_plusplus_mpo/blob/main/mpo.cpp" target="_blank" rel="noopener noreferrer">
           <img src={image2} alt="The MPO Algorithm Written in C++"/>
         </a>
-        <p>Implementation of the MPO (RL) algorithm in C++.</p>
+        <p>Implementation of the MPO (RL) algorithm in C++</p>
       </div>
       <div className="image-box">
         <a href="https://jjmobility.net" target="_blank" rel="noopener noreferrer">
           <img src={image3} alt="A front end site"/>
         </a>
-        <p>Front-end React site for a local business.</p>
+        <p>Front-end React site for a local business</p>
       </div>
     </div>
   );
